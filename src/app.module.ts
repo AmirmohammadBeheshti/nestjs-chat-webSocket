764712be861c21576ead96e7c9ce8e2ff98db87e
AmirmohammadBeheshti <bamir9707@gmail.com>
2023-04-09 01:12:11 +0430

    initial project

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GatewayModule } from './gateway/gateway.module';
+
+@Module({
+  imports: [GatewayModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
