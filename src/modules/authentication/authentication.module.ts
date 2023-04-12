cead7096c32bed6a6f109b8a31f09f8a48349689
AmirmohammadBeheshti <bamir9707@gmail.com>
2023-04-12 15:16:54 +0430

    add prisma module

--- src/modules/authentication/authentication.module.ts
+++ src/modules/authentication/authentication.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
+import { PrismaModule } from '../shared/modules/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [AuthenticationService],
   controllers: [AuthenticationController],
 })
